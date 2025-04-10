473e8091c06ab782227e65d685caf5a7ae2226ea
Brandon Boch López <alejandrobochlopez@gmail.com>
2025-04-10 11:00:38 -0600

    Mantenimiento

---
+++ Codigo/Modulos/ExamenParcialDos/Modelo_Vista_Parcialdos/Cursos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modelo_Vista_Parcialdos
+{
+    public partial class Cursos : Form
+    {
+        public Cursos()
+        {
+            InitializeComponent();
+            string idUsuario = Interfac_V3.UsuarioSesion.GetIdUsuario();
+
+            // ---------------------------------- Brandon Boch ----------------------------------
+            // Utilizando navegador
+            string[] alias = { "Id Curso", "Curso", "estado" };
+            navegador1.AsignarAlias(alias);
+            navegador1.AsignarSalida(this);
+            navegador1.AsignarColorFondo(ColorTranslator.FromHtml("#B4D2F0"));
+            navegador1.AsignarColorFuente(Color.Black);
+            navegador1.AsignarTabla("cursos");
+            navegador1.ObtenerIdAplicacion("1000");
+            navegador1.ObtenerIdUsuario(idUsuario);
+            navegador1.AsignarAyuda("1");
+            navegador1.AsignarNombreForm("");
+        }
+    }
+}
